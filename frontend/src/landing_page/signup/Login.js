2b7379f0cd346072dfd11ca6ef0dcd49924c7342
Vikash kumar <vikashkumar843399@gmail.com>
2026-02-23 11:46:55 +0530

    Update redirect URL after successful login

--- frontend/src/landing_page/signup/Login.js
+++ frontend/src/landing_page/signup/Login.js
@@ -43,7 +43,7 @@ const Login = () => {
         handleSuccess(message);
         setTimeout(() => {
           // navigate("/");
-          window.location.href = "https://zerodha-clone-frontend-s33j.onrender.com";
+          window.location.href = "https://zerodha-clone-dashboard-mpmt.onrender.com";
         }, 1000);
       } else {
         handleError(message);
